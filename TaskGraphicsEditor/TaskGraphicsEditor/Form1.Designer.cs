fce6112bca1b3900cbf461e2f65e4fbcdd2d9d94
Nick <avtodrivevid@gmail.com>
2016-05-30 01:45:00 +0300

    add project files

---
+++ TaskGraphicsEditor/TaskGraphicsEditor/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace TaskGraphicsEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.InvertButton4 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.CircleButton8 = new System.Windows.Forms.Button();
+            this.RectButton7 = new System.Windows.Forms.Button();
+            this.LineButton6 = new System.Windows.Forms.Button();
+            this.PencilButton5 = new System.Windows.Forms.Button();
+            this.DeleteButton3 = new System.Windows.Forms.Button();
+            this.SaveBbutton2 = new System.Windows.Forms.Button();
+            this.OpenButton1 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(589, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 16);
+            this.label1.TabIndex = 3;
+            // 
+            // InvertButton4
+            // 
+            this.InvertButton4.Location = new System.Drawing.Point(180, 12);
+            this.InvertButton4.Name = "InvertButton4";
+            this.InvertButton4.Size = new System.Drawing.Size(114, 30);
+            this.InvertButton4.TabIndex = 7;
+            this.InvertButton4.Text = "Инвертирование";
+            this.InvertButton4.UseVisualStyleBackColor = true;
+            this.InvertButton4.Click += new System.EventHandler(this.InvertButton4_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // CircleButton8
+            // 
+            this.CircleButton8.Image = global::TaskGraphicsEditor.Properties.Resources.ellip;
+            this.CircleButton8.Location = new System.Drawing.Point(468, 12);
+            this.CircleButton8.Name = "CircleButton8";
+            this.CircleButton8.Size = new System.Drawing.Size(50, 30);
+            this.CircleButton8.TabIndex = 11;
+            this.CircleButton8.UseVisualStyleBackColor = true;
+            this.CircleButton8.Click += new System.EventHandler(this.CircleButton8_Click);
+            // 
+            // RectButton7
+            // 
+            this.RectButton7.Image = global::TaskGraphicsEditor.Properties.Resources.rect;
+            this.RectButton7.Location = new System.Drawing.Point(412, 12);
+            this.RectButton7.Name = "RectButton7";
+            this.RectButton7.Size = new System.Drawing.Size(50, 30);
+            this.RectButton7.TabIndex = 10;
+            this.RectButton7.UseVisualStyleBackColor = true;
+            this.RectButton7.Click += new System.EventHandler(this.RectButton7_Click);
+            // 
+            // LineButton6
+            // 
+            this.LineButton6.Image = global::TaskGraphicsEditor.Properties.Resources.line;
+            this.LineButton6.Location = new System.Drawing.Point(356, 12);
+            this.LineButton6.Name = "LineButton6";
+            this.LineButton6.Size = new System.Drawing.Size(50, 30);
+            this.LineButton6.TabIndex = 9;
+            this.LineButton6.UseVisualStyleBackColor = true;
+            this.LineButton6.Click += new System.EventHandler(this.LineButton6_Click);
+            // 
+            // PencilButton5
+            // 
+            this.PencilButton5.Image = global::TaskGraphicsEditor.Properties.Resources.pencil;
+            this.PencilButton5.Location = new System.Drawing.Point(300, 12);
+            this.PencilButton5.Name = "PencilButton5";
+            this.PencilButton5.Size = new System.Drawing.Size(50, 30);
+            this.PencilButton5.TabIndex = 8;
+            this.PencilButton5.UseVisualStyleBackColor = true;
+            this.PencilButton5.Click += new System.EventHandler(this.PencilButton5_Click);
+            // 
+            // DeleteButton3
+            // 
+            this.DeleteButton3.Image = global::TaskGraphicsEditor.Properties.Resources.Delete;
+            this.DeleteButton3.Location = new System.Drawing.Point(124, 12);
+            this.DeleteButton3.Name = "DeleteButton3";
+            this.DeleteButton3.Size = new System.Drawing.Size(50, 30);
+            this.DeleteButton3.TabIndex = 6;
+            this.DeleteButton3.UseVisualStyleBackColor = true;
+            this.DeleteButton3.Click += new System.EventHandler(this.DeleteButton3_Click);
+            // 
+            // SaveBbutton2
+            // 
+            this.SaveBbutton2.Image = global::TaskGraphicsEditor.Properties.Resources.Save;
+            this.SaveBbutton2.Location = new System.Drawing.Point(68, 12);
+            this.SaveBbutton2.Name = "SaveBbutton2";
+            this.SaveBbutton2.Size = new System.Drawing.Size(50, 30);
+            this.SaveBbutton2.TabIndex = 5;
+            this.SaveBbutton2.UseVisualStyleBackColor = true;
+            this.SaveBbutton2.Click += new System.EventHandler(this.SaveBbutton2_Click);
+            // 
+            // OpenButton1
+            // 
+            this.OpenButton1.Image = global::TaskGraphicsEditor.Properties.Resources.Open;
+            this.OpenButton1.Location = new System.Drawing.Point(12, 12);
+            this.OpenButton1.Name = "OpenButton1";
+            this.OpenButton1.Size = new System.Drawing.Size(50, 30);
+            this.OpenButton1.TabIndex = 4;
+            this.OpenButton1.UseVisualStyleBackColor = true;
+            this.OpenButton1.Click += new System.EventHandler(this.OpenButton1_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Black;
+            this.pictureBox4.Location = new System.Drawing.Point(623, 48);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(54, 46);
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(653, 70);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(54, 46);
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkGray;
+            this.pictureBox1.Image = global::TaskGraphicsEditor.Properties.Resources._011;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 48);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(570, 381);
+            this.pictureBox1.MinimumSize = new System.Drawing.Size(570, 381);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(570, 381);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(524, 12);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(183, 30);
+            this.progressBar.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 441);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.CircleButton8);
+            this.Controls.Add(this.RectButton7);
+            this.Controls.Add(this.LineButton6);
+            this.Controls.Add(this.PencilButton5);
+            this.Controls.Add(this.InvertButton4);
+            this.Controls.Add(this.DeleteButton3);
+            this.Controls.Add(this.SaveBbutton2);
+            this.Controls.Add(this.OpenButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(735, 480);
+            this.MinimumSize = new System.Drawing.Size(545, 370);
+            this.Name = "Form1";
+            this.Text = "Graphics Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button OpenButton1;
+        private System.Windows.Forms.Button SaveBbutton2;
+        private System.Windows.Forms.Button DeleteButton3;
+        private System.Windows.Forms.Button InvertButton4;
+        private System.Windows.Forms.Button PencilButton5;
+        private System.Windows.Forms.Button LineButton6;
+        private System.Windows.Forms.Button RectButton7;
+        private System.Windows.Forms.Button CircleButton8;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ TaskGraphicsEditor/TaskGraphicsEditor/Form1.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using TaskGraphicsEditor.Properties;
+
+namespace TaskGraphicsEditor
+{
+    public partial class Form1 : Form
+    {
+        #region Constructors
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeBitmap();
+        }
+        #endregion
+
+        #region Fields
+        private bool cancel_Flag = false;
+        private int shape_Mode;
+        private int mode;
+        private Point motion_Point;
+        private Point first_Point;
+        private Point null_Point = new Point(int.MaxValue, 0);
+        private Pen pen = new Pen(Color.Black);
+        private SolidBrush fill_Brush = new SolidBrush(Color.White);
+        private bool equal_Dimension;
+        private Items.Item CurrentItem;
+        private Bitmap _bitmap;
+        private event Action<int> ProcessChanging;
+        private event Action<bool> InvertComplete;
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Launch ProcessChanging and InvertComplete delegates.
+        /// Launch the counter for progressBar
+        /// </summary>
+        private void VisualizationProgressInverting()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (cancel_Flag)
+                    break;
+                Thread.Sleep(25);
+                ProcessChanging(i);
+            }
+            InvertComplete(cancel_Flag);
+        }
+        /// <summary>
+        /// The primary Bitmap object initialization 
+        /// </summary>
+        private void InitializeBitmap()
+        {
+            try
+            {
+                _bitmap = (Bitmap)Bitmap.FromFile(@"..\..\Resources\01.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                pictureBox1.Image = _bitmap;
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show(Resources.error1);
+            }
+        }
+        /// <summary>
+        /// Drawing shape, accepting parameters Rectangle and Graphics
+        /// </summary>
+        /// <param name="r"></param>
+        /// <param name="g"></param>
+        private void DrawShape(Rectangle r, Graphics g)
+        {
+            switch (shape_Mode)
+            {
+                case 0:
+                    if (CurrentItem == Items.Item.Rectangle)
+                        g.FillRectangle(fill_Brush, r);
+                    g.DrawRectangle(pen, r);
+                    break;
+                case 1:
+                    if (CurrentItem == Items.Item.Circle)
+                        g.FillEllipse(fill_Brush, r);
+                    g.DrawEllipse(pen, r);
+                    break;
+            }
+        }
+        /// <summary>
+        /// Rectangle drawing method,
+        /// accepting parameters two points, returns new Rectangle
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <returns></returns>
+        private Rectangle DrawRectMethod(Point p1, Point p2)
+        {
+            if (equal_Dimension)
+            {
+                int dx = p2.X - p1.X, dy = p2.Y - p1.Y;
+                if (Math.Abs(dx) > Math.Abs(dy))
+                    p2.X = p1.X + Math.Sign(dx) * Math.Abs(dy);
+                else
+                    p2.Y = p1.Y + Math.Sign(dy) * Math.Abs(dx);
+            }
+            int x = Math.Min(p1.X, p2.X),
+                y = Math.Min(p1.Y, p2.Y),
+                w = Math.Abs(p2.X - p1.X),
+                h = Math.Abs(p2.Y - p1.Y);
+            return new Rectangle(x, y, w, h);
+        }
+        private void DrawMode()
+        {
+            Point p1 = pictureBox1.PointToScreen(first_Point),
+                p2 = pictureBox1.PointToScreen(motion_Point);
+            if (mode == 1)
+                ControlPaint.DrawReversibleLine(p1, p2, Color.Black);
+            else
+                ControlPaint.DrawReversibleFrame(DrawRectMethod(p1, p2), Color.Black, (FrameStyle)((shape_Mode + 1) % 2));
+
+        }
+        /// <summary>
+        /// Setting the property Value of progressBar control
+        /// Sending the Action delegate to the Invoke method
+        /// Launch Invoke method
+        /// </summary>
+        /// <param name="progress"></param>
+        private void ProcessChange(int progress)
+        {
+            Action action = () =>
+            {
+                progressBar.Value = progress;
+            };
+            try
+            {
+                Invoke(action);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Image invertion, restoring the property Enabled for the button InvertButton4
+        /// Sending the Action delegate to the Invoke method
+        /// Launch Invoke method
+        /// </summary>
+        /// <param name="cenceled"></param>
+        private void Completed(bool cenceled)
+        {
+            Action action = () =>
+            {
+                InvertButton4.Enabled = true;
+                _bitmap.RotateFlip(RotateFlipType.Rotate180FlipY);
+                pictureBox1.Image = _bitmap;
+            };
+            try
+            {
+                Invoke(action);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        #endregion
+
+        #region EventHandlers
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            motion_Point = first_Point = e.Location;
+            if (Control.ModifierKeys == Keys.Alt)
+            {
+                Color c = (pictureBox1.Image as Bitmap).GetPixel(e.X, e.Y);
+                if (e.Button == MouseButtons.Left)
+                    pictureBox4.BackColor = c;
+                else
+                    pictureBox3.BackColor = c;
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = string.Format("X,Y:{0},{1}", e.X, e.Y);
+            if (first_Point == null_Point)
+                return;
+            if (e.Button == MouseButtons.Left)
+                switch (mode)
+                {
+                    case 0:
+                        Graphics g = Graphics.FromImage(pictureBox1.Image);
+                        g.DrawLine(pen, first_Point, e.Location);
+                        g.Dispose();
+                        first_Point = e.Location;
+                        pictureBox1.Invalidate();
+                        break;
+                    case 1:
+                    case 2:
+                        DrawMode();
+                        motion_Point = e.Location;
+                        equal_Dimension = Control.ModifierKeys == Keys.Control;
+                        DrawMode();
+                        break;
+                }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (first_Point == null_Point)
+                return;
+            if (mode >= 1)
+            {
+                Graphics g = Graphics.FromImage(pictureBox1.Image);
+                switch (mode)
+                {
+                    case 1:
+                        g.DrawLine(pen, first_Point, motion_Point);
+                        break;
+                    case 2:
+                        DrawShape(DrawRectMethod(first_Point, motion_Point), g);
+                        break;
+                }
+                g.Dispose();
+                pictureBox1.Invalidate();
+
+            }
+        }
+        private void PencilButton5_Click(object sender, EventArgs e)
+        {
+            CurrentItem = Items.Item.Pencil;
+            mode = 0;
+        }
+        private void LineButton6_Click(object sender, EventArgs e)
+        {
+            CurrentItem = Items.Item.Line;
+            mode = 1;
+        }
+
+        private void RectButton7_Click(object sender, EventArgs e)
+        {
+            CurrentItem = Items.Item.Rectangle;
+            mode = 2;
+            shape_Mode = 0;
+        }
+
+        private void CircleButton8_Click(object sender, EventArgs e)
+        {
+            CurrentItem = Items.Item.Circle;
+            mode = 2;
+            shape_Mode = 1;
+        }
+        private void DeleteButton3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Очистить рисунок?", "Очистка", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+ == DialogResult.Yes)
+            {
+                using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+                {
+                    g.Clear(Color.White);
+                }
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void SaveBbutton2_Click(object sender, EventArgs e)
+        {
+            first_Point = null_Point;
+
+            string s0 = saveFileDialog.FileName;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string s = saveFileDialog.FileName;
+                if (s.ToUpper() == s0.ToUpper())
+                {
+                    s0 = Path.GetDirectoryName(s0) + ".png";
+                    pictureBox1.Image.Save(s0);
+                    pictureBox1.Image.Dispose();
+                    File.Delete(s);
+                    File.Move(s0, s);
+                    pictureBox1.Image = new Bitmap(s);
+                }
+                else
+                    pictureBox1.Image.Save(s);
+                Text = "Graphics Editor-" + s;
+            }
+        }
+
+        private void OpenButton1_Click(object sender, EventArgs e)
+        {
+            first_Point = null_Point;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string s = openFileDialog.FileName;
+                try
+                {
+                    Image im = new Bitmap(s);
+                    Graphics g = Graphics.FromImage(im);
+                    g.Dispose();
+                    if (pictureBox1.Image != null)
+                        pictureBox1.Image.Dispose();
+                    pictureBox1.Image = im;
+                }
+                catch
+                {
+                    MessageBox.Show("File" + s + " has a wrong format.", "Error");
+                    return;
+                }
+                Text = "Image Editor-" + s;
+                saveFileDialog.FileName = Path.ChangeExtension(s, "png");
+                openFileDialog.FileName = "";
+            }
+        }
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            PictureBox pb = sender as PictureBox;
+            colorDialog1.Color = pb.BackColor;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pb.BackColor = colorDialog1.Color;
+            pen.Color = pictureBox4.BackColor;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            PictureBox pb = sender as PictureBox;
+            colorDialog1.Color = pb.BackColor;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pb.BackColor = colorDialog1.Color;
+            fill_Brush.Color = pictureBox3.BackColor;
+        }
+
+        private void InvertButton4_Click(object sender, EventArgs e)
+        {
+            Form1 f = new Form1();
+            f.ProcessChanging += ProcessChange;
+            f.InvertComplete += Completed;
+            InvertButton4.Enabled = false;
+            Thread th = new Thread(f.VisualizationProgressInverting);
+            th.Start();
+        }
+
+        #endregion
+    }
+}
